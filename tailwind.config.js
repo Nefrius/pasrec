7e085b014e0b08d9d92bc47bf89d82bd9a65ebb2
Nefrius <enesbas616161@gmail.com>
2024-08-05 00:16:17 +0300

    new ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+
+      fontFamily: {
+        'gega-baslik': ["Montserrat", "sans-serif"]
+      },
+
+
+      colors:{
+        'gega-gradient-1': '#787171',
+        'gega-gradient-2': '#8f8686',
+        'gega-circle-2': '#737880',
+        'gega-circle-3': '#D6EFD8',
+      }
+    },
+  
   },
   plugins: [],
 }
