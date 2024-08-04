54716eb4acd14ef1e95766a8e5076e575ee3ed8a
Nefrius <enesbas616161@gmail.com>
2024-08-05 00:26:34 +0300

    add new color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,8 @@ module.exports = {
       colors:{
         'gega-gradient-1': '#787171',
         'gega-gradient-2': '#8f8686',
-        'gega-circle-2': '#737880',
-        'gega-circle-3': '#D6EFD8',
+        'gega-circle-2': '#508C9B',
+        'gega-circle-3': '#134B70',
       }
     },
   
